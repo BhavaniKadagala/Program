fa4782c7e57250b059d5b15a2300d8fa6977c129
BhavaniKadagala <113003160+BhavaniKadagala@users.noreply.github.com>
2022-10-28 10:55:51 +0530

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,6 +1,5 @@
 class Animal {
-  String name;
-  public void eat() {
-    System.out.println("I can eat");
-  }
+    public static void main(String[] args) {
+        System.out.println("This is Lion"); 
+    }
 }
